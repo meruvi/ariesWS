d6b1984b891ccd49d0e8aa9e05bf7bbffb47a5e5
meruvi <gmeruviarojas@gmail.com>
2019-10-06 09:13:36 -0400

    ORIGIN

---
+++ src/main/java/com/meruvia/ariesWS/AriesWsApplication.java
@@ -0,0 +1,13 @@
+package com.meruvia.ariesWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriesWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meruvia/ariesWS/AriesWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.meruvia.ariesWS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AriesWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
